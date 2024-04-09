1124c3d791b7952e344f389e5cf6beed98e3f5d7
Chris Andriessen <50437199+ChrisAnd1998@users.noreply.github.com>
2024-04-09 09:02:15 +0200

    Add files via upload

--- mat2lb.php
+++ mat2lb.php
@@ -22,7 +22,53 @@ foreach ($data as $entry) {
 
     //if (str_contains($entry['thickness'], 'mm')) {
         $xmlString = '<Material name="'.$entry['name'].'">';
-        $xmlString .= '<Entry Thickness="-1.0000" Desc="Speed: '.$entry['speedFineRate'].' Pierce: '.$entry['pierceTime'].'" NoThickTitle="'.$entry['thickness'].'">';
+        $xmlString .= '<Entry Thickness="-1.0000" Desc="Spd: '.$entry['speedRoughRate'].' (Rough) Pce: '.$entry['pierceTime'].'" NoThickTitle="'.$entry['thickness'].'">';
+        $xmlString .= '<CutSetting type="Cut">';
+        $xmlString .= '<index Value="0"/>';
+        $xmlString .= '<name Value=""/>';
+        $xmlString .= '<LinkPath Value="'.$entry['name'].'/'.$entry['thickness'].'/'.str_replace('&','and',$entry['category']).'"/>';
+        $xmlString .= '<minPower Value="4"/>';
+        $xmlString .= '<maxPower Value="100"/>';
+        $xmlString .= '<maxPower2 Value="20"/>';
+        $xmlString .= '<speed Value="' . ((str_replace('in/min','',(str_replace('mm/min','',$entry['speedRoughRate']))))) / 60 . '"/>';
+        $xmlString .= '<startDelay Value="' . str_replace('s','',$entry['pierceTime']) . '"/>';
+        $xmlString .= '<leadIn Value="1"/>';
+        $xmlString .= '<leadLength Value="5"/>';
+        $xmlString .= '<leadAngle Value="90"/>';
+        $xmlString .= '<leadStyle Value="arc"/>';
+        $xmlString .= '<priority Value="0"/>';
+        $xmlString .= '<tabCount Value="1"/>';
+        $xmlString .= '<tabCountMax Value="1"/>';
+        $xmlString .= '</CutSetting>';
+        $xmlString .= '</Entry>';
+        $xmlString .= '</Material>';
+
+
+        $xmlString .= '<Material name="'.$entry['name'].'">';
+        $xmlString .= '<Entry Thickness="-1.0000" Desc="Spd: '.$entry['speedMediumRate'].' (Medium) Pce: '.$entry['pierceTime'].'" NoThickTitle="'.$entry['thickness'].'">';
+        $xmlString .= '<CutSetting type="Cut">';
+        $xmlString .= '<index Value="0"/>';
+        $xmlString .= '<name Value=""/>';
+        $xmlString .= '<LinkPath Value="'.$entry['name'].'/'.$entry['thickness'].'/'.str_replace('&','and',$entry['category']).'"/>';
+        $xmlString .= '<minPower Value="4"/>';
+        $xmlString .= '<maxPower Value="100"/>';
+        $xmlString .= '<maxPower2 Value="20"/>';
+        $xmlString .= '<speed Value="' . ((str_replace('in/min','',(str_replace('mm/min','',$entry['speedMediumRate']))))) / 60 . '"/>';
+        $xmlString .= '<startDelay Value="' . str_replace('s','',$entry['pierceTime']) . '"/>';
+        $xmlString .= '<leadIn Value="1"/>';
+        $xmlString .= '<leadLength Value="5"/>';
+        $xmlString .= '<leadAngle Value="90"/>';
+        $xmlString .= '<leadStyle Value="arc"/>';
+        $xmlString .= '<priority Value="0"/>';
+        $xmlString .= '<tabCount Value="1"/>';
+        $xmlString .= '<tabCountMax Value="1"/>';
+        $xmlString .= '</CutSetting>';
+        $xmlString .= '</Entry>';
+        $xmlString .= '</Material>';
+
+
+        $xmlString .= '<Material name="'.$entry['name'].'">';
+        $xmlString .= '<Entry Thickness="-1.0000" Desc="Spd: '.$entry['speedFineRate'].' (Fine) Pce: '.$entry['pierceTime'].'" NoThickTitle="'.$entry['thickness'].'">';
         $xmlString .= '<CutSetting type="Cut">';
         $xmlString .= '<index Value="0"/>';
         $xmlString .= '<name Value=""/>';
